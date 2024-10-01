d906bb55b73291c860896cf7aeee98f4ab2118d3
Joaquim Alves <jafn92@gmail.com>
2024-10-01 16:11:10 -0300

    add stack router an app

---
+++ src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Index</Text>
+    </View>
+  );
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,13 @@
+import { colors } from "@/styles/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: colors.green[300],
+  },
+});
