cb0107dfc658b37191e9ebd72795a17b8116f762
Joaquim Alves <jafn92@gmail.com>
2024-10-01 16:51:49 -0300

    add header on app

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import { styles } from "./styles";
+import { colors } from "@/styles/colors";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Index</Text>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
