d698fa5f68df9aa7a9b454a28bd0bbd7d8f85c19
Joaquim Alves <jafn92@gmail.com>
2024-10-02 09:37:31 -0300

    feat: create modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,18 @@
-import { View, Image, TouchableOpacity, FlatList } from "react-native";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 
-import { Category } from "@/components/category";
-import { categories } from "@/utils/categories";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 
 export default function Index() {
   return (
@@ -34,6 +40,32 @@ export default function Index() {
         style={styles.links}
         contentContainerStyle={styles.linksContent}
       />
+
+      <Modal transparent visible>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>RocketSeat</Text>
+            <Text style={styles.modalUrl}>https://teste.com</Text>
+
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
